73cb274444f51ac44590d8a7d0c3cdc72d8d956a
Zhihan Yue <zhihan.yue@pku.edu.cn>
2021-06-23 10:16:41 +0800

    cleanup

--- tasks/forecasting.py
+++ tasks/forecasting.py
@@ -6,13 +6,10 @@ def generate_pred_samples(features, data, pred_len, drop=0):
     n = data.shape[1]
     features = features[:, :-pred_len]
     labels = np.stack([ data[:, i:1+n+i-pred_len] for i in range(pred_len)], axis=2)[:, 1:]
-    baseline = np.expand_dims(data[:, :-pred_len], 2).repeat(pred_len, axis=2)
     features = features[:, drop:]
     labels = labels[:, drop:]
-    baseline = baseline[:, drop:]
     return features.reshape(-1, features.shape[-1]), \
-            labels.reshape(-1, labels.shape[2]*labels.shape[3]), \
-            baseline.reshape(-1, baseline.shape[2]*baseline.shape[3])
+            labels.reshape(-1, labels.shape[2]*labels.shape[3])
 
 def cal_metrics(pred, target):
     return {
@@ -42,14 +39,13 @@ def eval_forecasting(model, data, train_slice, valid_slice, test_slice, scaler, 
     test_data = data[:, test_slice, n_covariate_cols:]
     
     ours_result = {}
-    baseline_result = {}
     lr_train_time = {}
     lr_infer_time = {}
     out_log = {}
     for pred_len in pred_lens:
-        train_features, train_labels, train_baseline = generate_pred_samples(train_repr, train_data, pred_len, drop=padding)
-        valid_features, valid_labels, valid_baseline = generate_pred_samples(valid_repr, valid_data, pred_len)
-        test_features, test_labels, test_baseline = generate_pred_samples(test_repr, test_data, pred_len)
+        train_features, train_labels = generate_pred_samples(train_repr, train_data, pred_len, drop=padding)
+        valid_features, valid_labels = generate_pred_samples(valid_repr, valid_data, pred_len)
+        test_features, test_labels = generate_pred_samples(test_repr, test_data, pred_len)
         
         t = time.time()
         lr = eval_protocols.fit_ridge(train_features, train_labels, valid_features, valid_labels)
@@ -61,16 +57,13 @@ def eval_forecasting(model, data, train_slice, valid_slice, test_slice, scaler, 
 
         ori_shape = test_data.shape[0], -1, pred_len, test_data.shape[2]
         test_pred = test_pred.reshape(ori_shape)
-        test_baseline = test_baseline.reshape(ori_shape)
         test_labels = test_labels.reshape(ori_shape)
         
         if test_data.shape[0] > 1:
             test_pred_inv = scaler.inverse_transform(test_pred.swapaxes(0, 3)).swapaxes(0, 3)
-            test_baseline_inv = scaler.inverse_transform(test_baseline.swapaxes(0, 3)).swapaxes(0, 3)
             test_labels_inv = scaler.inverse_transform(test_labels.swapaxes(0, 3)).swapaxes(0, 3)
         else:
             test_pred_inv = scaler.inverse_transform(test_pred)
-            test_baseline_inv = scaler.inverse_transform(test_baseline)
             test_labels_inv = scaler.inverse_transform(test_labels)
             
         out_log[pred_len] = {
@@ -83,14 +76,9 @@ def eval_forecasting(model, data, train_slice, valid_slice, test_slice, scaler, 
             'norm': cal_metrics(test_pred, test_labels),
             'raw': cal_metrics(test_pred_inv, test_labels_inv)
         }
-        baseline_result[pred_len] = {
-            'norm': cal_metrics(test_baseline, test_labels),
-            'raw': cal_metrics(test_baseline_inv, test_labels_inv)
-        }
         
     eval_res = {
         'ours': ours_result,
-        'baseline': baseline_result,
         'ts2vec_infer_time': ts2vec_infer_time,
         'lr_train_time': lr_train_time,
         'lr_infer_time': lr_infer_time
